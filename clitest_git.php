43354446e291ebbcf9a55a8a6db8323a48050f47
pboldizs <peter.boldizs@disney.com>
2013-10-03 13:54:01 +0200

    into master

--- clitest_git.php
+++ clitest_git.php
@@ -5,6 +5,7 @@
 //soft reset
 //now testing stash, stash@{2}
 //branch test: before create
+//branch test: after creating learngit branch
 list (,$firstname) =$argv;
 echo "Hello from command line: $firstname\n";
 ?>
